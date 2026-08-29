d77cef1e9e5ff563ab7d94d533a58158489390df
shwetadraut01-web <shwetadraut01@gmail.com>
2026-08-29 22:43:50 +0530

    Added my first java code

---
+++ java/pract.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello AWS - My First Website!");
+        
+        // Simple logic
+        int a = 10;
+        int b = 20;
+        System.out.println("Sum is: " + (a + b));
+    }
+}
