939ae567129aa9fc55187b9a8cbea996d4214fb7
shwetadraut01-web <shwetadraut01@gmail.com>
2026-08-30 22:21:38 +0530

    twosum practice

---
+++ java/2p.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+            if (map.containsKey(need)) {
+                return new int[]{map.get(need), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] result = twoSum(new int[]{2, 7, 11, 15}, 9);
+        System.out.println(Arrays.toString(result)); // [0, 1]
+    }
+}
